63838ec2b209141953457a7efd1cc2581f700e25
Junsang Moon <mun.js@aol.com>
2025-04-01 19:47:08 +0900

    feat: change copyright

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,7 @@
 <footer class="bg-zinc-950 text-zinc-100 p-4 mt-8">
     <div class="container mx-auto text-center">
-        <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
+        <p>
+            &copy; {new Date().getFullYear()} junsang.dev. All rights reserved.
+        </p>
     </div>
 </footer>
