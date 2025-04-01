5584332a8776e271f27ac42bb6244f232ce6d5d8
Junsang Moon <mun.js@aol.com>
2025-04-01 17:13:00 +0900

    feat: typo & scrolling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,81 @@
 <script>
-    import Header from "$lib/componenets/Header.svelte";
-    import Footer from "$lib/componenets/Footer.svelte";
-    import Card from "$lib/componenets/Card.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import Card from "$lib/components/Card.svelte";
 
     const posts = [
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
+        {
+            title: "Hello World",
+            summary: "This is the first post on my new blog.",
+            date: "10/10/2020",
+            link: "/posts/hello-world",
+        },
+        {
+            title: "My Second Post",
+            summary: "This is the second post on my new blog.",
+            date: "10/11/2020",
+            link: "/posts/my-second-post",
+        },
         {
             title: "Hello World",
             summary: "This is the first post on my new blog.",
@@ -19,18 +91,18 @@
     ];
 </script>
 
-<Header title="My Blog" />
-
-<main class="container mx-auto h-svh p-4">
-    <h1 class="text-3xl font-bold mb-6">Latest Posts</h1>
-    {#each posts as post}
-        <Card
-            title={post.title}
-            summary={post.summary}
-            date={post.date}
-            link={post.link}
-        />
-    {/each}
-</main>
-
-<Footer />
+<div class="flex flex-col h-screen">
+    <Header title="My Blog" />
+    <main class="flex-1 overflow-y-auto container mx-auto p-4">
+        <h1 class="text-3xl font-bold mb-6">Latest Posts</h1>
+        {#each posts as post}
+            <Card
+                title={post.title}
+                summary={post.summary}
+                date={post.date}
+                link={post.link}
+            />
+        {/each}
+    </main>
+    <Footer />
+</div>
