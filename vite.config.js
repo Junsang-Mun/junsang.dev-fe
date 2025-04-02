39957d12918558ce89c340ff9ff160e22b8d783e
Junsang Moon <mun.js@aol.com>
2025-04-03 02:01:59 +0900

    fix: vite.config.js now load .env.production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [sveltekit()],
+    define: {
+      "import.meta.env.VITE_GITHUB_CLIENT_ID": JSON.stringify(
+        env.VITE_GITHUB_CLIENT_ID,
+      ),
+      "import.meta.env.VITE_GITHUB_CLIENT_SECRET": JSON.stringify(
+        env.VITE_GITHUB_CLIENT_SECRET,
+      ),
+      "import.meta.env.VITE_REDIRECT_URI": JSON.stringify(
+        env.VITE_REDIRECT_URI,
+      ),
+      "import.meta.env.VITE_ADMIN_EMAIL": JSON.stringify(env.VITE_ADMIN_EMAIL),
+    },
+  };
 });
